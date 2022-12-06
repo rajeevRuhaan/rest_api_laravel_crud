bba9e8999c4467215e8351818262168f29705ae7
rajeevRuhaan <urrajivin@hotmail.com>
2022-12-06 04:12:09 +0200

    learning CRUD api in laravel

---
+++ app/Http/Controllers/CountryController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCountryRequest;
+use App\Http\Resources\CountryResource;
+use App\Http\Resources\CountryCollection;
+use App\Http\Resources\CountyCollection;
+use App\Models\Countries;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CountryController extends Controller
+{
+    public function index()
+    {
+        return new CountyCollection(Countries::paginate(10));
+    }
+
+    public function show(Countries $country)
+    {
+        return new CountryResource($country);
+    }
+
+    public function store(StoreCountryRequest $request)
+    {
+        Countries::create($request->validated());
+        return response()->json('Countries Created');
+    }
+    
+    public function update(StoreCountryRequest $request, Countries $country)
+    {
+        $country-> update($request-> validated());
+        return response()-> json('countries upated');
+    }
+    
+    public function destroy(Countries $country)
+    {
+        $country->delete();
+        return response()-> json('country deleted');
+    }
+}
