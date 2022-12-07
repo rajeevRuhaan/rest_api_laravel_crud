021114d0c9eb2ef262b4073862ea91a175cbf279
rajeevRuhaan <urrajivin@hotmail.com>
2022-12-07 14:36:23 +0200

    Product collection resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,7 +17,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return new ProductCollection(Product::paginate(10)) ;
+        return ProductCollection::collection(Product::paginate(10)) ;
     }
 
     /**
--- app/Http/Resources/ProductCollection.php
+++ app/Http/Resources/ProductCollection.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class ProductCollection extends ResourceCollection
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductCollection extends JsonResource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +15,17 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return[
+        'name'=> $this->name,
+        'discount'=> $this->discount,
+        'totalPrice' => round(((1-($this->discount/100)) * $this->price), 2),
+        'rating'=> $this->reviews->count() > 0 ?
+        round($this->reviews->sum('star')/$this->reviews->count(), 2)
+        : 'No rating yet',
+        'href' => [
+            'link' => route('products.show', $this->id)
+        ]
+
+        ];
     }
 }
